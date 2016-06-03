d57fdd49e40466ca1ccc062c0500098dd822651a
Hari <hari.kongu@gmail.com>
2016-06-03 13:00:19 +0530

    tried for loop in haskell

---
+++ haskell_programming/variables.hs
@@ -0,0 +1,16 @@
+main = do
+    let a = "initial"
+    putStrLn a
+
+    let b = 1
+    let c = 2
+    print b >> print c
+
+    let d = True
+    print d
+
+    let e = undefined :: Int
+    print e
+
+    let f = "short"
+    putStrLn f
